bd917ee1a1edacdfc7b6edebc9eb3ba1d9cd2bde
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-03-07 23:05:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Course_8
+{
+    internal class Program
+    {
+        public enum Renkler { Kirmizi, Yesil, Mavi }
+        static void Main(string[] args)
+        {
+            Renkler r = (Renkler)(new Random()).Next(0, 10);
+
+            switch (r)
+            {
+                case Renkler.Kirmizi:
+                    Console.WriteLine("Renk kirmizidir.");
+                    break;
+                case Renkler.Yesil:
+                    Console.WriteLine("Renk yesildir.");
+                    break;
+                case Renkler.Mavi:
+                    Console.WriteLine("Renk mavidir.");
+                    break;
+                default:
+                    Console.WriteLine("Renk bilinmiyor.");
+                    break;
+
+            }
+        }
+    }
+}
